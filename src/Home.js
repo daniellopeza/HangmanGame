e87465f8079687430fbe63b31ad70e2b122b4f46
Daniel Lopez <daniellopez@email.arizona.edu>
2019-03-28 16:46:03 -0700

    Getting user input

--- src/Home.js
+++ src/Home.js
@@ -7,43 +7,58 @@ class Home extends Component {
     super(props);
 
     this.state = {
+      // TODO: Feed word via props
       word: ["D", "a", "r", "l", "y"],
-      guess: ["_", "_", "_", "_", "_"]
+      guess: ["_", "_", "_", "_", "_"],
+      letterGuess: ''
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  async handleSubmit(event){
+    event.preventDefault(); // prevents refresh after submittion
+
+    const {word, guess} = this.state;
+    console.log("user guessed = ", this.state.letterGuess)
+    
+
+    // const form = await axios.post('/api/form', {
+    //     name, // == name: name;
+    //     email,
+    //     systemType,
+    //     rSelected,
+    // })
+  }
 
   render() {
-      console.log("Rendering Home page")
     return (
         
       <div className="Home">
-
         <header className="App-header">
           <p> Hangman</p>
 
-          <table  id= "myTable">
-            <tbody>
-                <tr>
-                    {this.state.guess.map(item => (
-                    <th key={item}>{item}</th>
-                    ))}
-                </tr>
-            </tbody>
+            <table id= "myTable">
+                <tbody>
+                    <tr>
+                        {this.state.guess.map(item => (
+                        <th key={item}>{item}</th>
+                        ))}
+                    </tr>
+                </tbody>
             </table>
 
-            <form>
-            <br></br>
-            Guess a letter: 
-            <input type="text" name="guess"/>
-            <input type="submit" value="Submit"/>
+            <form name="guessForm" method="POST" onSubmit={this.handleSubmit}>
+                <br></br>
+                Guess a letter: 
+                <input type="text" name="letterGuess" onChange={this.handleChange}/>
+                <input type="submit" value="submit" onChange={this.handleChange}/>
             </form>
-
         </header>
-        
-        
-
-
       </div>
     );
   }
