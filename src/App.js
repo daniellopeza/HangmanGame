248d51c35f911ebae5052941a371cd636450b53b
Daniel Lopez <daniellopez@email.arizona.edu>
2019-03-28 15:15:50 -0700

    CreateReactApp

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    console.log("Rendering App Component\n")
+    return (
+      <Router>
+        {/* need divs when using multiple components */}
+        <div> 
+            <Route exact path="/" component={Home} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
