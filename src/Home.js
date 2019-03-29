6c17c4f45118c3385a05d00656eaf8344a65f941
Daniel Lopez <daniellopez@email.arizona.edu>
2019-03-28 19:07:23 -0700

    Added tablesize bugfix comments

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
 
     this.state = {
       // TODO: Feed word via props
-      word: ["D", "a", "r", "l", "y"],
+      word: ["C", "l", "o", "u", "d"],
       guess: ["_", "_", "_", "_", "_"],
       letterGuess: ''
     };
@@ -27,19 +27,12 @@ class Home extends Component {
     updateItem = i => {
         this.setState(state => {
             const guess = state.guess.map((item, j) => {
-                console.log("i = ", i)
-                console.log("j = ", j)
-                
-                if (j == i) {
-                    console.log("Setting char to ", this.state.word[i])
+                if (j === i) {
                     return this.state.word[i];
                 } else {
-                    console.log("no ", i)
                     return item;
                 }
             });
-
-            console.log("guess = ", guess)
             return {
                 guess,
             };
@@ -50,30 +43,17 @@ class Home extends Component {
     event.preventDefault(); // prevents refresh after submittion
 
     const {word, guess} = this.state;
-    console.log("user guessed = ", this.state.letterGuess)
-    console.log("length       = ", this.state.letterGuess.length)
 
     if (this.state.letterGuess.length > 1){
         alert("Please enter 1 character.")
         return;
     }
 
-    console.log("word       = ", this.state.word)
     let i = 0;
     while(i < this.state.word.length){
         if(this.state.word[i].toLowerCase() == this.state.letterGuess.toLowerCase()){
             console.log("Good Job! Guessed one letter.")
-            console.log("setting " , i , " to ", this.state.word[i])
-            //this.setState({guess: []})
             this.updateItem(i)
-            // let guess = this.state.guess;
-            // guess[i] = this.state.word[i]//{...guess[i], key: this.state.word[i]};
-            // this.setState({
-            //     guess: guess
-            // })
-            // this.state.guess[i] = this.state.word[i]
-            // this.forceUpdate()
-            //this.setState({guess: this.state.guess})
         }
         i++;
     }
@@ -87,7 +67,6 @@ class Home extends Component {
 
 
   render() {
-      //console.log("rendering, guess = ", this.state.guess)
     return (
         
       <div className="Home">
@@ -109,30 +88,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-
-    // return (
-        
-    //   <div className="Home">
-    //     <header className="App-header">
-    //       <p> Hangman</p>
-    //       {console.log("state rendering = ", this.state.guess)}
-    //         <table id= "myTable">
-    //             <tbody>
-    //                 <tr>
-    //                     {this.state.guess.map(item => (
-    //                      <th key={item}>{item}</th>
-    //                     ))}
-    //                 </tr>
-    //             </tbody>
-    //         </table>
-
-    //         <form name="guessForm" method="POST" onSubmit={this.handleSubmit}>
-    //             <br></br>
-    //             Guess a letter: 
-    //             <input type="text" name="letterGuess" onChange={this.handleChange}/>
-    //             <input type="submit" value="submit" onChange={this.handleChange}/>
-    //         </form>
-    //     </header>
-    //   </div>
-    // );
--- src/table.js
+++ src/table.js
@@ -8,16 +8,19 @@ class Table extends Component {
 
   render() {
     return (
-        // <div>
-        //     {this.props.guess.map((item, index) => (
-        //     <li key={index}>{item}</li>
-        // ))}
-        // </div>
       <div >
-          {console.log("state rendering = ", this.props.guess)}
             <table id= "myTable">
                 <tbody>
                     <tr>
+                        {/* 
+                        had a terrible bug that kept rendering a larger table
+                        than the actual size of the list "this.props.guess", after updating
+                        the list is when this error would occur. 
+
+                        FIX: each element needs a unique key, otherwise it would not know the 
+                        difference between new and old elements, rendering the old too. 
+                        The error was "<th key={item}>{item}</th>", where the key was not unique.
+                        */}
                         {this.props.guess.map((item, index)  => (
                          <th key={index}>{item}</th>
                         ))}
