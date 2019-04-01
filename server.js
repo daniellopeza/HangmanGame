31e64bc03c55a35d382b11f42dec11b0085c34ab
Daniel Lopez <daniellopez@email.arizona.edu>
2019-03-31 21:07:08 -0700

    Quick edit

--- server.js
+++ server.js
@@ -7,19 +7,21 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var MongoClient = require('mongodb').MongoClient;
 //Create a database named "mydb":
+var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
+
     console.log("Database created!");
     var mydb = db.db("mydb")
+
+    // store possible game words into db
     var words = [
         { word: "Cloud"},
         { word: "Quantum"}
         ];
-  
     mydb.collection("words").insertMany(words, function(err, res) {
         if (err) throw err;
         console.log("Number of documents inserted: " + res.insertedCount);
@@ -28,7 +30,6 @@ MongoClient.connect(url, function(err, db) {
 });
 
 app.use(express.static(__dirname +'./index.html')); //serves the index.html
-
 
 app.listen(4000, () => {
     console.log(`Server listening on port 4000`)
@@ -40,8 +41,7 @@ app.get('/gameWord', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    console.log("hitting / route")
-
+    // Query for the word to be used in Hangman game
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var mydb = db.db("mydb")
@@ -49,9 +49,10 @@ app.get('*', (req, res) => {
         mydb.collection("words").findOne({}, function(err, result) {
             if (err) throw err;
             console.log('game word = ', result.word);
+            res.send(result.word)
             db.close();
         });
 
     });
-    res.send({})
+
 });
